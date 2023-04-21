d16983d9749f79727efeafb0e64e1ac308f74308
hszhrr <ffelicia918@gmail.com>
2023-04-21 22:27:18 +0700

    Sign up

---
+++ ProjekISA_01_SVF_ManajemenArtis/ProjekISA_01_SVF_ManajemenArtis/FormSignUp.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace ProjekISA_01_SVF_ManajemenArtis
+{
+    partial class FormSignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLokasi = new System.Windows.Forms.TextBox();
+            this.dateTimePickerAcara = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPengaju = new System.Windows.Forms.TextBox();
+            this.textBoxJudul = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.comboBoxPosisi = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBoxLokasi
+            // 
+            this.textBoxLokasi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLokasi.Location = new System.Drawing.Point(231, 335);
+            this.textBoxLokasi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxLokasi.Name = "textBoxLokasi";
+            this.textBoxLokasi.Size = new System.Drawing.Size(563, 35);
+            this.textBoxLokasi.TabIndex = 42;
+            // 
+            // dateTimePickerAcara
+            // 
+            this.dateTimePickerAcara.CalendarFont = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerAcara.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerAcara.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerAcara.Location = new System.Drawing.Point(231, 163);
+            this.dateTimePickerAcara.Name = "dateTimePickerAcara";
+            this.dateTimePickerAcara.Size = new System.Drawing.Size(244, 40);
+            this.dateTimePickerAcara.TabIndex = 41;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(64, 337);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 33);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 33);
+            this.label5.TabIndex = 39;
+            this.label5.Text = "Username";
+            // 
+            // textBoxPengaju
+            // 
+            this.textBoxPengaju.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPengaju.Location = new System.Drawing.Point(231, 274);
+            this.textBoxPengaju.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxPengaju.Name = "textBoxPengaju";
+            this.textBoxPengaju.Size = new System.Drawing.Size(563, 35);
+            this.textBoxPengaju.TabIndex = 38;
+            // 
+            // textBoxJudul
+            // 
+            this.textBoxJudul.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxJudul.Location = new System.Drawing.Point(231, 110);
+            this.textBoxJudul.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxJudul.Name = "textBoxJudul";
+            this.textBoxJudul.Size = new System.Drawing.Size(563, 35);
+            this.textBoxJudul.TabIndex = 37;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 33);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Tanggal Lahir";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 33);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(291, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 56);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Sign Up";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 33);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Posisi";
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTambah.Location = new System.Drawing.Point(636, 405);
+            this.buttonTambah.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(158, 48);
+            this.buttonTambah.TabIndex = 45;
+            this.buttonTambah.Text = "Daftar";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxPosisi
+            // 
+            this.comboBoxPosisi.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPosisi.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxPosisi.FormattingEnabled = true;
+            this.comboBoxPosisi.Location = new System.Drawing.Point(231, 222);
+            this.comboBoxPosisi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBoxPosisi.Name = "comboBoxPosisi";
+            this.comboBoxPosisi.Size = new System.Drawing.Size(563, 35);
+            this.comboBoxPosisi.TabIndex = 46;
+            // 
+            // FormSignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 482);
+            this.Controls.Add(this.comboBoxPosisi);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLokasi);
+            this.Controls.Add(this.dateTimePickerAcara);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPengaju);
+            this.Controls.Add(this.textBoxJudul);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormSignUp";
+            this.Text = "Sign Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLokasi;
+        private System.Windows.Forms.DateTimePicker dateTimePickerAcara;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPengaju;
+        private System.Windows.Forms.TextBox textBoxJudul;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.ComboBox comboBoxPosisi;
+    }
+}
