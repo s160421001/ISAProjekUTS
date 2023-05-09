2ca3984acbe8821feb801fd1e72ff6c8e103db3c
s160421001 <stchrist03@gmail.com>
2023-05-09 16:25:56 +0700

    t

--- ProjekISA_01_SVF_ManajemenArtis/ProjekISA_01_SVF_ManajemenArtis/FormDaftarJadwalArtis.Designer.cs
+++ ProjekISA_01_SVF_ManajemenArtis/ProjekISA_01_SVF_ManajemenArtis/FormDaftarJadwalArtis.Designer.cs
@@ -44,10 +44,10 @@ namespace ProjekISA_01_SVF_ManajemenArtis
             this.label1.BackColor = System.Drawing.Color.Navy;
             this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Location = new System.Drawing.Point(24, 19);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(796, 55);
+            this.label1.Size = new System.Drawing.Size(708, 44);
             this.label1.TabIndex = 76;
             this.label1.Text = "JADWAL ARTIS";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -55,88 +55,93 @@ namespace ProjekISA_01_SVF_ManajemenArtis
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.White;
             this.radioButton4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(402, 161);
+            this.radioButton4.Location = new System.Drawing.Point(357, 129);
             this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(228, 31);
+            this.radioButton4.Size = new System.Drawing.Size(191, 27);
             this.radioButton4.TabIndex = 73;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Nama Acara (Z-A)";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.UseVisualStyleBackColor = false;
             this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.White;
             this.radioButton3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(155, 161);
+            this.radioButton3.Location = new System.Drawing.Point(138, 129);
             this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(228, 31);
+            this.radioButton3.Size = new System.Drawing.Size(191, 27);
             this.radioButton3.TabIndex = 72;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Nama Acara (A-Z)";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.UseVisualStyleBackColor = false;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.White;
             this.radioButton2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(402, 112);
+            this.radioButton2.Location = new System.Drawing.Point(357, 90);
             this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(271, 31);
+            this.radioButton2.Size = new System.Drawing.Size(219, 27);
             this.radioButton2.TabIndex = 71;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Tanggal Ditambahkan";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseVisualStyleBackColor = false;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.White;
             this.radioButton1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(155, 112);
+            this.radioButton1.Location = new System.Drawing.Point(138, 90);
             this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(224, 31);
+            this.radioButton1.Size = new System.Drawing.Size(182, 27);
             this.radioButton1.TabIndex = 70;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Tanggal Kegiatan";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseVisualStyleBackColor = false;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(25, 109);
+            this.label2.Location = new System.Drawing.Point(22, 87);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 33);
+            this.label2.Size = new System.Drawing.Size(103, 29);
             this.label2.TabIndex = 69;
             this.label2.Text = "Urutkan";
             // 
             // dataGridViewJadwal
             // 
             this.dataGridViewJadwal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewJadwal.Location = new System.Drawing.Point(27, 218);
+            this.dataGridViewJadwal.Location = new System.Drawing.Point(24, 174);
             this.dataGridViewJadwal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewJadwal.Name = "dataGridViewJadwal";
             this.dataGridViewJadwal.RowHeadersWidth = 62;
             this.dataGridViewJadwal.RowTemplate.Height = 28;
-            this.dataGridViewJadwal.Size = new System.Drawing.Size(796, 438);
+            this.dataGridViewJadwal.Size = new System.Drawing.Size(708, 350);
             this.dataGridViewJadwal.TabIndex = 68;
             this.dataGridViewJadwal.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewJadwal_CellContentClick);
             // 
             // FormDaftarJadwalArtis
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProjekISA_01_SVF_ManajemenArtis.Properties.Resources.Bg_basic;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(850, 695);
+            this.ClientSize = new System.Drawing.Size(756, 556);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.radioButton3);
@@ -144,7 +149,6 @@ namespace ProjekISA_01_SVF_ManajemenArtis
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dataGridViewJadwal);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormDaftarJadwalArtis";
             this.Text = "Jadwal";
             this.Load += new System.EventHandler(this.FormDaftarJadwalArtis_Load);
--- ProjekISA_01_SVF_ManajemenArtis/ProjekISA_01_SVF_ManajemenArtis/FormDaftarJadwalArtisYangDiManage.Designer.cs
+++ ProjekISA_01_SVF_ManajemenArtis/ProjekISA_01_SVF_ManajemenArtis/FormDaftarJadwalArtisYangDiManage.Designer.cs
@@ -45,100 +45,107 @@ namespace ProjekISA_01_SVF_ManajemenArtis
             // dataGridViewJadwal
             // 
             this.dataGridViewJadwal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewJadwal.Location = new System.Drawing.Point(33, 255);
+            this.dataGridViewJadwal.Location = new System.Drawing.Point(29, 204);
             this.dataGridViewJadwal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridViewJadwal.Name = "dataGridViewJadwal";
             this.dataGridViewJadwal.RowHeadersWidth = 62;
             this.dataGridViewJadwal.RowTemplate.Height = 28;
-            this.dataGridViewJadwal.Size = new System.Drawing.Size(796, 438);
+            this.dataGridViewJadwal.Size = new System.Drawing.Size(708, 350);
             this.dataGridViewJadwal.TabIndex = 9;
             this.dataGridViewJadwal.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewJadwal_CellContentClick);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 146);
+            this.label2.Location = new System.Drawing.Point(27, 117);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 33);
+            this.label2.Size = new System.Drawing.Size(103, 29);
             this.label2.TabIndex = 10;
             this.label2.Text = "Urutkan";
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.White;
             this.radioButton1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(161, 150);
+            this.radioButton1.Location = new System.Drawing.Point(143, 120);
             this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(224, 31);
+            this.radioButton1.Size = new System.Drawing.Size(182, 27);
             this.radioButton1.TabIndex = 11;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Tanggal Kegiatan";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseVisualStyleBackColor = false;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.White;
             this.radioButton2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(407, 150);
+            this.radioButton2.Location = new System.Drawing.Point(362, 120);
             this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(271, 31);
+            this.radioButton2.Size = new System.Drawing.Size(219, 27);
             this.radioButton2.TabIndex = 12;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Tanggal Ditambahkan";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseVisualStyleBackColor = false;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.White;
             this.radioButton3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(161, 199);
+            this.radioButton3.Location = new System.Drawing.Point(143, 159);
             this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(228, 31);
+            this.radioButton3.Size = new System.Drawing.Size(191, 27);
             this.radioButton3.TabIndex = 13;
             this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Nama Acara (A-Z)";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.UseVisualStyleBackColor = false;
             this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
+            this.radioButton4.BackColor = System.Drawing.Color.White;
             this.radioButton4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(407, 199);
+            this.radioButton4.Location = new System.Drawing.Point(362, 159);
             this.radioButton4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(228, 31);
+            this.radioButton4.Size = new System.Drawing.Size(191, 27);
             this.radioButton4.TabIndex = 14;
             this.radioButton4.TabStop = true;
             this.radioButton4.Text = "Nama Acara (Z-A)";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.UseVisualStyleBackColor = false;
             this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(30, 98);
+            this.label3.Location = new System.Drawing.Point(27, 78);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 33);
+            this.label3.Size = new System.Drawing.Size(67, 29);
             this.label3.TabIndex = 15;
             this.label3.Text = "Artis";
             // 
             // comboBoxArtis
             // 
+            this.comboBoxArtis.BackColor = System.Drawing.Color.White;
             this.comboBoxArtis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxArtis.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxArtis.FormattingEnabled = true;
-            this.comboBoxArtis.Location = new System.Drawing.Point(161, 95);
+            this.comboBoxArtis.Location = new System.Drawing.Point(143, 76);
             this.comboBoxArtis.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBoxArtis.Name = "comboBoxArtis";
-            this.comboBoxArtis.Size = new System.Drawing.Size(492, 40);
+            this.comboBoxArtis.Size = new System.Drawing.Size(438, 34);
             this.comboBoxArtis.TabIndex = 18;
             this.comboBoxArtis.SelectedIndexChanged += new System.EventHandler(this.comboBoxArtis_SelectedIndexChanged);
             // 
@@ -147,21 +154,22 @@ namespace ProjekISA_01_SVF_ManajemenArtis
             this.label1.BackColor = System.Drawing.Color.Navy;
             this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(33, 16);
+            this.label1.Location = new System.Drawing.Point(29, 13);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(796, 55);
+            this.label1.Size = new System.Drawing.Size(708, 44);
             this.label1.TabIndex = 67;
             this.label1.Text = "JADWAL ARTIS YANG DI MANAGE";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormDaftarJadwalArtisYangDiManage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::ProjekISA_01_SVF_ManajemenArtis.Properties.Resources.Bg_basic;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(856, 722);
+            this.ClientSize = new System.Drawing.Size(761, 578);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxArtis);
             this.Controls.Add(this.label3);
